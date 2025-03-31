7aebdf0b5139b5247e6a10162bc449fa5fdbc48e
Fernando Ramirez <fernandoramga@gmail.com>
2025-03-30 22:55:48 -0600

    commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,6 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import staticSite from '@astrojs/static-site';
 
 export default defineConfig({
-  site: 'https://raga-2027.github.io/DigitalIntech/',  // Cambia esto por tu URL real
-  base: '/DigitalIntech/',  // Asegúrate de usar el nombre de tu repositorio
-  output: 'static',
   integrations: [tailwind()],
-  adapter: staticSite(),
 });
-

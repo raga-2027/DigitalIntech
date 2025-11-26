5e870af114c590a25953ed124659d213413562e7
Fernando Ramirez <fernandoramga@gmail.com>
2025-11-25 23:12:47 -0600

    Version 1.9

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,8 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  site: 'https://raga-2027.github.io/',
-  base: '/',
+  site: 'https://raga-2027.github.io/DigitallIntech/',
+  base: '/DigitallIntech/',
 
   integrations: [tailwind()]
 });
